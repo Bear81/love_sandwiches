3403c8ba45e8f32c8abea198caf05626f100a7fc
Bear81 <142313344+Bear81@users.noreply.github.com>
2024-01-09 10:49:30 +0000

    Refactor update worksheet functions

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
-from pprint import pprint
 import gspread
 from google.oauth2.service_account import Credentials
+from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -13,94 +13,108 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
+
 def get_sales_data():
-    '''
-    Get Sales figures input from the user
-    Run a While loop to collect a valid string of data from the user via the
-    terminal, which must be a string of 6 separated numbers (Separated by 
-    commas). Loop will repeat until valid entry is made
-    '''
+    """
+    Get sales figures input from the user.
+    Run a while loop to collect a valid string of data from the user
+    via the terminal, which must be a string of 6 numbers separated
+    by commas. The loop will repeatedly request data, until it is valid.
+    """
     while True:
-        print("Please enter sales data from the last market")
-        print("Data should be in six numbers , separated by commas.")
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
         print("Example: 10,20,30,40,50,60\n")
 
-        data_str = input("Enter Data here:")
-        
+        data_str = input("Enter your data here: ")
+
         sales_data = data_str.split(",")
-        validate_data(sales_data)
 
         if validate_data(sales_data):
             print("Data is valid!")
             break
-    return sales_data    
+
+    return sales_data
+
 
 def validate_data(values):
-    '''
+    """
     Inside the try, converts all string values into integers.
-    Raises ValueError if string connot be converted into int,
-    or if there aren't exactly 6 values
-    '''
+    Raises ValueError if strings cannot be converted into int,
+    or if there aren't exactly 6 values.
+    """
     try:
         [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
-                f"Eaxactly 6 values required, you provided {len(values)}"
+                f"Exactly 6 values required, you provided {len(values)}"
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
-        print(values)
         return False
 
     return True
 
+
 def update_sales_worksheet(data):
-    '''
-    Update sales worksheet, add new row with the list
-    '''
+    """
+    Update sales worksheet, add new row with the list data provided
+    """
     print("Updating sales worksheet...\n")
     sales_worksheet = SHEET.worksheet("sales")
     sales_worksheet.append_row(data)
-    print("Sales worksheet updated successfully")
+    print("Sales worksheet updated successfully.\n")
+
+
+def update_surplus_worksheet(data):
+    """
+    Update surplus worksheet, add new row with the list data provided
+    """
+    print("Updating surplus worksheet...\n")
+    surplus_worksheet = SHEET.worksheet("surplus")
+    surplus_worksheet.append_row(data)
+    print("Surplus worksheet updated successfully.\n")
+
+def update_worksheet(data, worksheet):
+    """
+    Received a list of intergers to be inserted into a worksheet.
+    update the relevant worksheet with the data provided
+    """
+    print(f"Updating {worksheet} worksheet")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} worksheet updated successfully\n")
 
 def calculate_surplus_data(sales_row):
-    '''
+    """
     Compare sales with stock and calculate the surplus for each item type.
 
     The surplus is defined as the sales figure subtracted from the stock:
-    -Positive surplis indicates waste
-    -Negitave surplies indicates extra made when stock was sold out
-    '''
-    print("Calculating surplus data ...\n")
+    - Positive surplus indicates waste
+    - Negative surplus indicates extra made when stock was sold out.
+    """
+    print("Calculating surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
-
+    
     surplus_data = []
     for stock, sales in zip(stock_row, sales_row):
         surplus = int(stock) - sales
         surplus_data.append(surplus)
-    
+
     return surplus_data
 
-def update_surplus_worksheet(data):
-    '''
-    Update surplus worksheet, add new row with the list
-    '''
-    print("Updating surplus worksheet...\n")
-    surplus_worksheet = SHEET.worksheet("surplus")
-    surplus_worksheet.append_row(data)
-    print("Sales worksheet updated successfully")
 
 def main():
-    '''
+    """
     Run all program functions
-    '''
+    """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
-    update_sales_worksheet(sales_data)
+    update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
-    update_surplus_worksheet(new_surplus_data)
-    
+    update_worksheet(new_surplus_data, "surplus")
 
-print("Welcome to love Sandwiches Data Automation")
+
+print("Welcome to Love Sandwiches Data Automation")
 main()
